61f01a5f192704fd56e52f61447cfdc1bbe19505
spenclark <spenclark@hotmail.com>
2020-03-10 14:12:17 -0700

    08-09

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,6 +2,11 @@
 
 # YOUR CODE HERE
 
+
+def if_even(num):
+    if num %
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -9,4 +14,3 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
